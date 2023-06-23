cf2e28e9d72bc038e1568196955a05e005c5be48
bharath21101 <rockybharth232@gmail.com>
2023-06-23 16:21:03 +0530

    newly edited changes

--- hello.java
+++ hello.java
@@ -1,8 +1,3 @@
-public class Narrowing
-{
-    public static void main( string [] Args){
-        double a=123.123;
-        int b=(int)a;
-        system.out
-    }
-}
+jsbdkebukebdaj
+aewkfjwejkf
+ehwfk.wbfjlwe.fmew
